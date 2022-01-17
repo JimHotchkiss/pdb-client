5e8723a9b551a6676fc4beb69f6e6b750def6a8d
Jimmy <jimmy.hotchkiss@gmail.com>
2022-01-16 19:19:32 -0500

    Got my api fetch working

--- src/adapters/brief.js
+++ src/adapters/brief.js
@@ -6,27 +6,19 @@ const searchBtnHandler = () => {
 const briefBtn = document.getElementById('brief-btn')
 const sectionOneImage = document.getElementById('section-one-img')
 briefBtn.addEventListener('click', () => {
-    sectionOneImage.src = "./styles/images/section1-logo3.svg"
-    // if (sectionOneImage.src == "./styles/images/section1-logo4.svg") {
-    //     console.log('if',sectionOneImage.src)
-    //     sectionOneImage.src = "./styles/images/section1-logo3.svg"
-    // } else {
-    //     console.log('else',sectionOneImage.src)
-    //     sectionOneImage.src = "./styles/images/section1-logo4.svg" 
-    // }
-    // const btnClassList = briefBtn.classList
-    // for(var i = 0; i < btnClassList.length; i++){
-    //     if(btnClassList[i] == 'btn-primary'){
-    //        briefBtn.classList.toggle('btn-danger')
-    //     }
-    // }
+    fetchBriefing()
     })
 }
 
 const fetchBriefing = () => {
     fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=bcd9264c4d4646b5a22d288a9a796d3d')
     .then(response => response.json())
-    .then(response => console.log(response.status, response.articles)
-    )
+    .then(response => fetchedData(response.articles))
+}
+
+const fetchedData = (newsData) => {
+    for(let item in newsData) {
+        console.log(newsData[item])
+    }
 }
 
